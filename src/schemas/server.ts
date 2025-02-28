1d95c5be17fc31047a3c15b3ca9e4fa9c1170b05
CrisWorld <52689017+CrisWorld@users.noreply.github.com>
2025-02-28 16:20:31 +0700

    fix invitelink schema validate

--- src/schemas/server.ts
+++ src/schemas/server.ts
@@ -10,13 +10,13 @@ export const InviteLinkSchema = z.object({
     required_error: "count is required",
     invalid_type_error: "count must be a number"
   }).refine((val) => val === -1 || (val > 0 && val < 30), {
-    message: "count must be -1 or between 1 and 29"
+    message: "count must be -1 or between 0 and 29"
   }),
 
   expireIn: z.number({
     required_error: "expireIn is required",
     invalid_type_error: "expireIn must be a valid date string"
-  }).min(1, {
+  }).min(0, {
     message: "expireIn must be greater than 0"
   }).max(30, {
     message: "expireIn must be less than 30"
